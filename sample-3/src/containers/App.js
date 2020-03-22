0bdedec2b84007ab5a3346dba9da3a8bd24376e8
Gordeev Nikita <nikita.kolibri@gmail.com>
2020-03-22 16:12:57 +0500

    [task][sample-3] Реализовал GraphComponent

--- sample-3/src/containers/App.js
+++ sample-3/src/containers/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import LayoutComponent from '../core/components/layoutComponent/LayoutComponent'
 import HeadingContainer from './heading/HeadingContainer'
 import TabsContainer from './tabs/TabsContainer'
+import GraphContainer from './graph/GraphContainer'
 
 export default () => (
     <LayoutComponent>
         <HeadingContainer />
         <TabsContainer />
+        <GraphContainer />
     </LayoutComponent>
 )
---
+++ sample-3/src/core/components/graphComponent/GraphComponent.js
@@ -0,0 +1,55 @@
+// @flow
+
+import React, { Node } from 'react'
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts'
+import styles from './graphComponent.module.css'
+
+type IGraphComponent = {
+    data: {
+        date: string,
+        value: number,
+    }[],
+    Controls: Node,
+}
+
+type ILineLabel = {
+    x: number,
+    y: number,
+    stroke: string,
+    value: number,
+}
+
+const LineLabel = ({ x, y, stroke, value }: ILineLabel) => (
+    <text
+        x={x}
+        y={y}
+        dy={-12}
+        fill={stroke}
+        className={styles.label}
+        textAnchor="middle"
+    >
+        {value}
+    </text>
+)
+
+const GraphComponent = ({ data, Controls }: IGraphComponent) => (
+    <div className={styles.wrapper}>
+        <LineChart
+            width={490}
+            height={225}
+            data={data}
+            className={styles.container}
+            margin={{
+                top: 20, right: 30, left: 20, bottom: 10,
+            }}
+        >
+            <CartesianGrid />
+            <XAxis dataKey="date" />
+            <YAxis domain={['auto', 'auto']} />
+            <Line dataKey="value" stroke="#cccccc" fill="#cccccc" label={<LineLabel />} />
+        </LineChart>
+        {Controls}
+    </div>
+)
+
+export default GraphComponent
